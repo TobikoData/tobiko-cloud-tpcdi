759efa8804b0545f51d1c111605fcd071d2cb0ed
Afzal Jasani <amj355@nyu.edu>
2024-09-18 17:11:47 -0700

    tcloud configs

---
+++ models/incremental/statustype.sql
@@ -0,0 +1,8 @@
+MODEL (
+  name tobiko_cloud_tpcdi.statustype,
+  kind FULL,
+);
+
+select *
+from tpcdi.tpcdi_raw_data_100.statustype
+;
---
+++ models/incremental/taxrate.sql
@@ -0,0 +1,9 @@
+MODEL (
+  name tobiko_cloud_tpcdi.taxrate,
+  kind FULL,
+);
+
+select *
+from tpcdi.tpcdi_raw_data_100.taxrate
+;
+
